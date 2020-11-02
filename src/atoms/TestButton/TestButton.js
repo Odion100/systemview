570a617add89e0aed6d6fc53a4eeb2109279f9c4
odion100 <odionedwards@gmail.com>
2020-11-02 16:53:09 -0500

    added sass files

---
+++ src/atoms/TestButton/TestButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+import fullcourtIcon from "../../assets/fullcourt.png";
+import hoopIcon from "../../assets/hoop.svg";
+import netIcon from "../../assets/hoop2.png";
+import "./styles.scss";
+
+const CourtActivityCard = ({ onSwitch }) => {
+  return (
+    <div><button>Test Button</button></div>
+
+  );
+};
+export default CourtActivityCard;
