52d02d54a44f869286071c48d96616c4d02e1ddb
odion100 <odionedwards@gmail.com>
2020-11-12 01:17:44 -0500

    added Text component test

---
+++ src/atoms/Text/Text.test.js
@@ -0,0 +1,22 @@
+/* eslint-disable jest/valid-expect */
+import React from "react";
+
+import { expect } from "chai";
+import { shallow, render } from "enzyme";
+
+import Text from "./Text";
+
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("<Text/>", () => {
+  it("renders a span tag with class text", () => {
+    const wrapper = shallow(<Text />);
+    expect(wrapper.find("span.text")).to.have.a.lengthOf(1);
+  });
+  it("use the text prop to render a span with innerText", () => {
+    const wrapper = render(<Text text="test" />);
+    expect(wrapper.text()).to.equal("test");
+  });
+});
