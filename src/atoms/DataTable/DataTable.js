0e244aef34334cc753d1c885ff1dde51a0b6f7ec
odion100 <odionedwards@gmail.com>
2020-12-08 03:38:47 -0500

    updated styling

--- src/atoms/DataTable/DataTable.js
+++ src/atoms/DataTable/DataTable.js
@@ -3,26 +3,24 @@ import "./styles.scss";
 
 const DataTable = ({ headers, table }) => {
   return (
-    <React.Fragment>
-      <table className="GeneratedTable">
-        <thead>
-          <tr>
-            {headers.map(({ name }, i) => (
-              <th key={i}>{name}</th>
+    <table className="data-table">
+      <thead>
+        <tr>
+          {headers.map(({ name }, i) => (
+            <th key={i}>{name}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {table.map((data, i) => (
+          <tr key={i}>
+            {data.map((value, i) => (
+              <td key={i}>{value}</td>
             ))}
           </tr>
-        </thead>
-        <tbody>
-          {table.map((data, i) => (
-            <tr key={i}>
-              {data.map((value, i) => (
-                <td key={i}>{value}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </React.Fragment>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
