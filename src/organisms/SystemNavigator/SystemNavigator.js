d14159603d36eb7c688f57e9427f6352eaaa9f50
odion100 <odionedwards@gmail.com>
2021-03-11 11:30:10 -0500

    move api search logic from page to organism

--- src/organisms/SystemNavigator/SystemNavigator.js
+++ src/organisms/SystemNavigator/SystemNavigator.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import ServiceContext from "../../ServiceContext";
 import "./styles.scss";
 import TextBox from "../../atoms/Textbox/Textbox";
 import Link from "../../atoms/Link/Link";
@@ -6,7 +7,20 @@ import ExpandableList from "../../molecules/ExpandableList/ExpandableList";
 import ServerModulesList from "../../organisms/ServerModulesList/ServerModulesList";
 import MissingDocIcon from "../../atoms/DocsIcon/DocsIcon";
 
-const SystemNav = ({ SearchInputSubmit, servicesList }) => {
+const SystemNav = () => {
+  const SystemViewAPI = useContext(ServiceContext);
+  const [servicesList, setServices] = useState([]);
+
+  const SearchInputSubmit = async (e) => {
+    const { SystemView } = SystemViewAPI;
+
+    try {
+      const results = await SystemView.getServices({ project_code: e.target.value });
+      if (results.length > 0) setServices(results.services);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <section className="system-nav">
       <div className="container">
--- src/pages/SystemViewer/SystemViewer.js
+++ src/pages/SystemViewer/SystemViewer.js
@@ -1,37 +1,14 @@
-import React, { useState, useContext } from "react";
-import ServiceContext from "../../ServiceContext";
+import React from "react";
 import "./styles.scss";
 import SystemNavigator from "../../organisms/SystemNavigator/SystemNavigator";
 import MethodDocumentation from "../../organisms/MethodDocumentation/MethodDocumentation";
 
 const SystemViewer = () => {
-  const SystemViewAPI = useContext(ServiceContext);
-  const [viewState, setViewState] = useState({ project_code: "", document_pointer: "" });
-  const [servicesList, setServices] = useState([]);
-
-  const SearchInputSubmit = async (e) => {
-    const { SystemView } = SystemViewAPI;
-
-    try {
-      const results = await SystemView.getServices({ project_code: e.target.value });
-      if (results.length > 0)
-        setViewState({
-          project_code: results[0].project_code,
-          document_pointer: "",
-        });
-      setServices(results.services);
-    } catch (error) {
-      console.log(error);
-    }
-
-    console.log(e.target.value);
-  };
-
   return (
     <section className="system-viewer">
       <div className="row">
         <div className="col-4">
-          <SystemNavigator SearchInputSubmit={SearchInputSubmit} servicesList={servicesList} />
+          <SystemNavigator />
         </div>
         <div className="col-6">
           <MethodDocumentation />
