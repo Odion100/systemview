7a1383f8aa4cc8e8a47957b21352b0b9b388d245
odion100 <odionedwards@gmail.com>
2021-03-11 11:04:53 -0500

    replace react router with hooksrouter module

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import SystemViewer from "./pages/SystemViewer/SystemViewer";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
-const App = ({ SystemViewAPI }) => {
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={(props) => <SystemViewer {...props} SystemViewAPI={SystemViewAPI} />}
-          />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
+import { useRoutes } from "hookrouter";
+
+const App = () =>
+  useRoutes({
+    "/": () => <SystemViewer />,
+  });
 
 export default App;
