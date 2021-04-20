fb62a0885878f28299746597d6f6a55f75b7a295
odion100 <odionedwards@gmail.com>
2021-04-20 14:48:00 -0400

    removed url parameters from route

--- src/App.js
+++ src/App.js
@@ -7,21 +7,9 @@ const Routes = {
     console.log(-1);
     return <SystemLink />;
   },
-  "/systemlink/:project": ({ project }) => {
+  "/systemlink/:project*": ({ project }) => {
     console.log(0);
-    return <SystemLink project={project} />;
-  },
-  "/systemlink/:project/:service": ({ project, service }) => {
-    console.log(1);
-    return <SystemLink project={project} service={service} />;
-  },
-  "/systemlink/:project/:service/:module": ({ project, service, module }) => {
-    console.log(2);
-    return <SystemLink project={project} service={service} module={module} />;
-  },
-  "/systemlink/:project/:service/:module/:method": ({ project, service, module, method }) => {
-    console.log(3);
-    return <SystemLink project={project} service={service} module={module} method={method} />;
+    return <SystemLink project_code={project} />;
   },
 };
 const App = () => {
