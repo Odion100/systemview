dc338a2cec5a2e9d0fb875530bfe5a1862b8696a
odion100 <odionedwards@gmail.com>
2021-09-08 17:05:38 -0400

    added and implemented ReactJson module

--- src/organisms/TestCenter/TestCenter.js
+++ src/organisms/TestCenter/TestCenter.js
@@ -1,8 +1,30 @@
 import React, { useState, useContext, useEffect } from "react";
-
-import Title from "../../atoms/Title/Title";
+import ReactJson from "react-json-view";
+import ObjectEditor from "../../molecules/ObjectEditor/ObjectEditor";
 import "./styles.scss";
-
+const mockData = {
+  _id: "6138caa3560d159aaf0c39b9",
+  index: 0,
+  guid: "f3e5241a-80a6-4765-8fe5-726a81514e3e",
+  isActive: true,
+  friends: [
+    {
+      id: 0,
+      name: "Sylvia Finch",
+    },
+    {
+      id: 1,
+      name: "Yolanda Dotson",
+    },
+    {
+      id: 2,
+      name: "Miriam Payne",
+    },
+  ],
+  greeting: "Hello, undefined! You have 5 unread messages.",
+  favoriteFruit: "apple",
+};
+const testfn = (data) => console.log(data);
 const TestCenter = ({ project_code, service_id, module_name, method_name }) => {
   return (
     <section className="test-center">
@@ -10,7 +32,16 @@ const TestCenter = ({ project_code, service_id, module_name, method_name }) => {
         <div className="row test-center__test-data">
           <span>
             {`${service_id}.${module_name}.${method_name}`}(
-            <span className="documentation-view__parameter">&#123;...&#125;</span>)
+            <ReactJson
+              name="data"
+              onAdd={testfn}
+              onEdit={testfn}
+              onDelete={testfn}
+              displayObjectSize={false}
+              displayDataTypes={false}
+              collapsed={true}
+            />
+            )
           </span>
         </div>
       </div>
