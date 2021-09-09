eccf9ad81864e9772185f7035d2fe7eeaf520381
odion100 <odionedwards@gmail.com>
2021-09-09 11:02:09 -0400

    created TestPanelCaption component and altred styling

---
+++ src/atoms/TestPanelCaption/TestPanelCaption.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.scss";
+
+const TestPanelCaption = ({ text }) => {
+  return <span className="test-panel-caption">{text}</span>;
+};
+
+export default TestPanelCaption;
--- src/organisms/TestDataSection/TestDataSection.js
+++ src/organisms/TestDataSection/TestDataSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactJson from "react-json-view";
 import ExpandableSection from "../../molecules/ExpandableSection/ExpandableSection";
 import AddButton from "../../atoms/AddButton/AddButton";
+import TestPanelCaption from "../../atoms/TestPanelCaption/TestPanelCaption";
 import "./styles.scss";
 
 const testfn = (data) => console.log(data);
@@ -10,9 +11,8 @@ const TestDataSection = ({ project_code, service_id, module_name, method_name })
     <section className="test-center-section">
       <ExpandableSection
         title={
-          <div className="d-flex">
-            <span className="test-center-section__title">Test Data:</span>
-
+          <div>
+            <TestPanelCaption text="Test Data:" />
             <AddButton hiddenCaption="json" />
           </div>
         }
