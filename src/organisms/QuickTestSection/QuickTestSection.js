c28ab3ae3224c88702572ec1c826d47a371b0267
odion100 <odionedwards@gmail.com>
2021-09-12 00:08:54 -0400

    Add scratchpad functionality to ScratcPad component

--- src/organisms/QuickTestSection/QuickTestSection.js
+++ src/organisms/QuickTestSection/QuickTestSection.js
@@ -1,9 +1,8 @@
-import React, { useContext, useEffect, useState } from "react";
+import React from "react";
 import ExpandableSection from "../../molecules/ExpandableSection/ExpandableSection";
 import TestPanelCaption from "../../atoms/TestPanelCaption/TestPanelCaption";
 import ScratchPad from "../ScratchPad/ScratchPad";
-import ServiceContext from "../../ServiceContext";
-import { Client } from "tasksjs-react-client";
+
 import "./styles.scss";
 
 const QuickTestSection = ({
@@ -15,32 +14,16 @@ const QuickTestSection = ({
   testData,
   title,
 }) => {
-  const { TestServices } = useContext(ServiceContext);
-  const connections = {};
-  connections[project_code] = {};
-  const [connected_services, setConnecteServices] = useState(connections);
-
-  useEffect(() => {
-    if (TestServices.length > 0) {
-      const service = TestServices.find(
-        (_service) => _service.project_code === project_code && _service.service_id === service_id
-      );
-      if (!service) return console.log("service not found");
-      if (!Client.loadedServices[service.url])
-        Client.loadService(service.url).then((_service) => {
-          connected_services[project_code][service_id] = _service;
-          setConnecteServices(connected_services);
-        });
-      else console.log("already loaded");
-    }
-  }, [project_code, service_id, TestServices]);
-
   return (
     <section className="quick-test-section">
       <ExpandableSection
         open={open}
         title_color="#0d8065"
-        title={<TestPanelCaption text={title} />}
+        title={
+          <React.Fragment>
+            <TestPanelCaption text={title} />{" "}
+          </React.Fragment>
+        }
       >
         <ScratchPad
           project_code={project_code}
--- src/organisms/ScratchPad/ScratchPad.js
+++ src/organisms/ScratchPad/ScratchPad.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ReactJson from "react-json-view";
 import JsonTextBox from "../../atoms/JsonTextBox/JsonTextBox";
+import TestsIcon from "../../atoms/TestsIcon/TestsIcon";
+import ServiceContext from "../../ServiceContext";
+import { Client } from "tasksjs-react-client";
 import "./styles.scss";
 
 const testfn = (data) => console.log(data);
-const TestDataSection = ({ project_code, service_id, module_name, method_name, testData }) => {
+const ScratchPad = ({ project_code, service_id, module_name, method_name, testData }) => {
+  const { TestServices } = useContext(ServiceContext);
+  const connections = {};
+  connections[project_code] = {};
+  const [connected_services, setConnecteServices] = useState(connections);
   const [showTxb, setShowTxb] = useState(false);
   const [jsonData, setJsonData] = useState(testData);
 
@@ -14,32 +21,63 @@ const TestDataSection = ({ project_code, service_id, module_name, method_name, t
     setJsonData(json);
     hideJsonTxb();
   };
+
+  const runTest = async () => {
+    try {
+      const results = await connected_services[project_code][service_id][module_name][method_name](
+        jsonData
+      );
+      console.log(results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    if (TestServices.length > 0) {
+      const service = TestServices.find(
+        (_service) => _service.project_code === project_code && _service.service_id === service_id
+      );
+      if (!service) return console.log("service not found");
+      if (!Client.loadedServices[service.url])
+        Client.loadService(service.url).then((_service) => {
+          connected_services[project_code][service_id] = _service;
+          setConnecteServices(connected_services);
+        });
+      else console.log("already loaded");
+    }
+  }, [project_code, service_id, TestServices]);
+
   return (
     <div className="scratchpad">
       <div className={`scratchpad__json-txb scratchpad__json-txb--show-${showTxb}`}>
         <JsonTextBox onSubmit={JsonTxbSubmit} onCancel={hideJsonTxb} obj={jsonData} />
       </div>
       <div className="scratchpad__test-data-container">
-        <span className="scratchpad__add-json-btn" onClick={showJsonTxb}>
-          +JSON
-        </span>
-        <div className="scratchpad__test-data"></div>
-        {`${service_id}.${module_name}.${method_name}`}(
-        <ReactJson
-          src={jsonData}
-          name="data"
-          onAdd={testfn}
-          onEdit={testfn}
-          onDelete={testfn}
-          displayObjectSize={false}
-          displayDataTypes={false}
-          collapsed={true}
-        />
-        )
+        <div className="scratchpad__btn-container">
+          <span className="scratchpad__add-json-btn" onClick={showJsonTxb}>
+            +JSON
+          </span>
+          <span className="scratchpad__run-test-btn" onClick={runTest}>
+            <TestsIcon isSaved={true} />
+          </span>
+        </div>
+        <div className="scratchpad__test-data">
+          {`${service_id}.${module_name}.${method_name}`}(
+          <ReactJson
+            src={jsonData}
+            name="data"
+            onAdd={testfn}
+            onEdit={testfn}
+            onDelete={testfn}
+            displayObjectSize={false}
+            displayDataTypes={false}
+            collapsed={true}
+          />
+          )
+        </div>
       </div>
-      <span>Run Test</span>
     </div>
   );
 };
 
-export default TestDataSection;
+export default ScratchPad;
