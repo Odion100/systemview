5cdf0e574a33280fe3ee481e116cd5f263c932d3
odion100 <odionedwards@gmail.com>
2022-05-21 23:46:43 -0400

    fixed misspellings

--- src/atoms/ExpandableIcon/ExpandableIcon.js
+++ src/atoms/ExpandableIcon/ExpandableIcon.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./styles.scss";
 
-const ExpandIcon = ({ isOpen, classname = "", onClick }) => {
+const ExpandIcon = ({ isOpen, className = "", onClick }) => {
   return isOpen ? (
-    <span onClick={onClick} className={`expandable-icon ${classname}`}>
+    <span onClick={onClick} className={`expandable-icon ${className}`}>
       &#9660;
     </span>
   ) : (
-    <span onClick={onClick} className={`expandable-icon ${classname}`}>
+    <span onClick={onClick} className={`expandable-icon ${className}`}>
       &#x25BA;
     </span>
   );
