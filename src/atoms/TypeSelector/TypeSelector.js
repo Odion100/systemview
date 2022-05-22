ba95246a774203ac32909a8e303dae24a57906c8
odion100 <odionedwards@gmail.com>
2022-05-21 23:34:16 -0400

    fixed misspelling and removed complex from list of options

--- src/atoms/TypeSelector/TypeSelector.js
+++ src/atoms/TypeSelector/TypeSelector.js
@@ -1,23 +1,13 @@
 import React from "react";
 import "./styles.scss";
 import Selector from "../Selector/Selector";
-const options = [
-  "string",
-  "number",
-  "date",
-  "boolean",
-  "object",
-  "undefined",
-  "null",
-  "target",
-  "complex",
-];
+const options = ["string", "number", "date", "boolean", "object", "undefined", "null", "target"];
 
-const TypeSelector = ({ default_type, onSelect, xClassname = "" }) => {
-  const classname = "type-selector";
+const TypeSelector = ({ default_type, onSelect, className = "" }) => {
+  const class_name = "type-selector";
   return (
     <Selector
-      className={`${classname} ${xClassname}`}
+      className={`${class_name} ${className}`}
       options={options}
       selected_option={default_type}
       onSelect={onSelect}
