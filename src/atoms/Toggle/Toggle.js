6cf589689e2e6cfee2aa4d80f5b8c344a67d51eb
odion100 <odionedwards@gmail.com>
2022-05-21 23:30:24 -0400

    changed defalutCheck to check

--- src/atoms/Toggle/Toggle.js
+++ src/atoms/Toggle/Toggle.js
@@ -4,7 +4,7 @@ import "./styles.scss";
 const Toggle = ({ round, isChecked, onChange }) => {
   return (
     <label className={`switch ${round ? "round" : ""}`}>
-      <input type="checkbox" defaultChecked={isChecked} onChange={onChange} />
+      <input type="checkbox" checked={isChecked} onChange={onChange} />
       <span className={`slider ${round ? "round" : ""}`}></span>
     </label>
   );
